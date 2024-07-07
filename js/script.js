8f63bad593e5c8ec8cb57cc2ca707e340730d241
gustavords <22622146+gustavords@users.noreply.github.com>
2024-07-07 14:12:49 -0400

    Add decimal and +/- button function to display() for floats and negatives

--- js/script.js
+++ js/script.js
@@ -38,8 +38,8 @@ const disableBtn = (btnNode) => {
 }
 
 const onlyOneDecimal = (num) => {
-    const decimalBtn = document.querySelector(`.options:last-child`); 
-    if(!Number.isInteger(num)){
+    const decimalBtn = document.querySelector(`#decimal`);
+    if (!Number.isInteger(num)) {
         disableBtn(decimalBtn);
     }
 }
@@ -57,16 +57,20 @@ function display() {
 
             displayText = display.textContent;
 
-            if (e.target.classList.contains(`num`)) {
+            if (e.target.classList.contains(`num`) || e.target.id == `decimal`) {
                 display.textContent += e.target.value;
             }
+            if (e.target.value == `-/+`) {
+                display.textContent = +display.textContent * (-1);
+            }
+
             if (num_a == `` && e.target.classList.contains(`operator`) && e.target.id != `equals`) {
                 num_a = displayText;
                 operator = e.target.value;
                 display.textContent = ``;
-                console.log(`num_a: ${num_a}`);
-                console.log(`num_b: ${num_b}`);
-                console.log(`operator: ${operator}`);
+                // console.log(`num_a: ${num_a}`);
+                // console.log(`num_b: ${num_b}`);
+                // console.log(`operator: ${operator}`);
             }
             else if (num_a != `` && num_b == `` && e.target.classList.contains(`operator`) && e.target.id != `equals`) {
                 num_b = displayText;
@@ -74,12 +78,10 @@ function display() {
                 num_b = ``;
                 operator = e.target.value;
                 display.textContent = ``;
-                console.log(`num_a: ${num_a}`);
-                console.log(`num_b: ${num_b}`);
-                // operatorNodes.forEach((node) => disableBtn(node));
+                // console.log(`num_a: ${num_a}`);
+                // console.log(`num_b: ${num_b}`);
             }
-            else if(num_a != `` && num_b == `` && e.target.id == `equals`){
-                debugger
+            else if (num_a != `` && num_b == `` && e.target.id == `equals`) {
                 num_b = displayText;
                 num_a = operate(operator, +num_a, +num_b);
                 display.textContent = num_a;
