12a264d5c13761b0a7d966a2564e7e4d8b38e4a9
gustavords <22622146+gustavords@users.noreply.github.com>
2024-07-07 14:48:53 -0400

    Add clear button function to display for clearing any calculations or evaluations on display

--- js/script.js
+++ js/script.js
@@ -55,22 +55,31 @@ function display() {
     buttons.forEach((btn) => {
         btn.addEventListener(`click`, (e) => {
 
-            displayText = display.textContent;
+            displayText = display.textContent;//<----- maybe should be elsewhere
 
             if (e.target.classList.contains(`num`) || e.target.id == `decimal`) {
+                // debugger;
+
                 display.textContent += e.target.value;
             }
             if (e.target.value == `-/+`) {
                 display.textContent = +display.textContent * (-1);
             }
 
+            if (e.target.value == `clear`) {
+                display.textContent = ``;
+                num_a = ``;
+                num_b = ``;
+                operator = ``;
+            }
+
             if (num_a == `` && e.target.classList.contains(`operator`) && e.target.id != `equals`) {
                 num_a = displayText;
                 operator = e.target.value;
                 display.textContent = ``;
-                // console.log(`num_a: ${num_a}`);
-                // console.log(`num_b: ${num_b}`);
-                // console.log(`operator: ${operator}`);
+                console.log(`num_a: ${num_a}`);
+                console.log(`num_b: ${num_b}`);
+                console.log(`operator: ${operator}`);
             }
             else if (num_a != `` && num_b == `` && e.target.classList.contains(`operator`) && e.target.id != `equals`) {
                 num_b = displayText;
@@ -78,8 +87,9 @@ function display() {
                 num_b = ``;
                 operator = e.target.value;
                 display.textContent = ``;
-                // console.log(`num_a: ${num_a}`);
-                // console.log(`num_b: ${num_b}`);
+                console.log(`num_a: ${num_a}`);
+                console.log(`num_b: ${num_b}`);
+                // operatorNodes.forEach((node) => disableBtn(node));
             }
             else if (num_a != `` && num_b == `` && e.target.id == `equals`) {
                 num_b = displayText;
@@ -90,59 +100,6 @@ function display() {
                 operator = ``;
             }
 
-
-
-
-
-
-            // console.log(`event start: ${displayText}`);
-
-            // if (displayText == `` && btn.classList.contains(`operator`)) {
-            //     // console.log(`display is empty and you pressed operator`);
-            // }
-
-            // if (!isNaN(e.target.value) && operator != ``) {
-            //     operatorNodes.forEach((node) => disableBtn(node));
-
-            // }
-
-            // else if (isNaN(e.target.value) && btn.classList.contains(`operator`) && displayText != ``) {
-            //     console.log(`!!!>${e.target.value}<!!! is an operator`);
-            //     displayText = display.textContent;
-
-            //     if (num_a == `` && operator == ``) {
-            //         console.log(`---num_a: ${num_a}`);
-            //         num_a = displayText;
-            //         operator = e.target.value;
-            //         display.textContent = ``;
-            //         console.log(`num_a: ${num_a}`);
-            //         console.log(`num_b: ${num_b}`);
-            //         console.log(`operator: ${operator}`);
-            //         // operatorNodes.forEach((node) => disableBtn(node));
-            //     }
-            //     else if (num_a != `` && num_b == ``) {
-            //         num_b = displayText;
-            //         num_a = operate(operator, +num_a, +num_b);
-            //         num_b = ``;
-            //         operator = e.target.value;
-            //         display.textContent = ``;
-            //         console.log(`num_a: ${num_a}`);
-            //         console.log(`num_b: ${num_b}`);
-            //         // operatorNodes.forEach((node) => disableBtn(node));
-            //     }
-            //     // else if (num_a != `` && num_b != ``) {
-            //     //     num_a = operate(e.target.value, +num_a, +num_b);
-            //     //     num_b = ``;
-            //     //     display.textContent = ``;
-            //     //     console.log(`num_a: ${num_a}`);
-            //     //     console.log(`num_b: ${num_b}`);
-            //     // }
-
-
-            //     // console.log(`num_a: ${num_a}`);
-            //     // console.log(`num_b: ${num_b}`);
-
-            // }
 
             // if (!isNaN(e.target.value) || e.target.value == `.` || e.target.value == `-/+`) {
             //     if (e.target.value == `-/+`) {
